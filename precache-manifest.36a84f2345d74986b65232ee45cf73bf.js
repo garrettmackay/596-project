178b5bb0c24e0c715596efab557a88ab5d30e228
Garrett MacKay <garrett.mackay@gmail.com>
2022-10-09 20:09:29 -0500

    Updates

---
+++ precache-manifest.36a84f2345d74986b65232ee45cf73bf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba2b4d2e52fc26147ba7",
+    "url": "/596-project/static/css/main.f515c7e5.chunk.css"
+  },
+  {
+    "revision": "ba2b4d2e52fc26147ba7",
+    "url": "/596-project/static/js/main.ba2b4d2e.chunk.js"
+  },
+  {
+    "revision": "2dc8e6cf5ce42dc8eb84",
+    "url": "/596-project/static/js/1.2dc8e6cf.chunk.js"
+  },
+  {
+    "revision": "338995ad5a24a88da760",
+    "url": "/596-project/static/js/2.338995ad.chunk.js"
+  },
+  {
+    "revision": "a433e7dc93efe7e04ed2",
+    "url": "/596-project/static/js/runtime~main.a433e7dc.js"
+  },
+  {
+    "revision": "e78522df31334f548cf1901b7d314b45",
+    "url": "/596-project/static/media/yana-almeida.e78522df.png"
+  },
+  {
+    "revision": "55ca610d33ed336064dacefe7c091e84",
+    "url": "/596-project/static/media/michael-adelin.55ca610d.jpg"
+  },
+  {
+    "revision": "0b13586051d3b22a7a452753797e44af",
+    "url": "/596-project/static/media/it-1.0b135860.jpg"
+  },
+  {
+    "revision": "195e36c5f0b19f28b8a999d93ff22a9c",
+    "url": "/596-project/static/media/diamond.195e36c5.jpg"
+  },
+  {
+    "revision": "badd5766cc95049f4b6f15363f358214",
+    "url": "/596-project/static/media/diamond-phishing.badd5766.png"
+  },
+  {
+    "revision": "14670a06212a36877bc33c708ed0803e",
+    "url": "/596-project/static/media/diamond-dos.14670a06.png"
+  },
+  {
+    "revision": "85d43ae0945b38905c700dc006b539eb",
+    "url": "/596-project/static/media/diamond-cc.85d43ae0.png"
+  },
+  {
+    "revision": "aa453ce82183efa511e895c2da04f7fd",
+    "url": "/596-project/static/media/diamond-fake.aa453ce8.png"
+  },
+  {
+    "revision": "0b8ee1481a4a33bcd8acef3eacee47a3",
+    "url": "/596-project/static/media/diamond-ransome.0b8ee148.png"
+  },
+  {
+    "revision": "3042f1e65ef7fa4cd4961816bd823268",
+    "url": "/596-project/static/media/phishing.3042f1e6.jpg"
+  },
+  {
+    "revision": "2d3bcdcde798c7c28408795e789e4c01",
+    "url": "/596-project/static/media/fraud.2d3bcdcd.jpg"
+  },
+  {
+    "revision": "c515878a6cb4716fda414af0688ef2a7",
+    "url": "/596-project/static/media/gas.c515878a.jpg"
+  },
+  {
+    "revision": "3d888a7d11248d458fd3754b1ee506cb",
+    "url": "/596-project/static/media/person.3d888a7d.png"
+  },
+  {
+    "revision": "2c68b2a8a021f8484393ee843966cb04",
+    "url": "/596-project/static/media/ransom.2c68b2a8.jpg"
+  },
+  {
+    "revision": "bd2b6cd19a2882eace0afd652b3b7a2a",
+    "url": "/596-project/static/media/truck-2.bd2b6cd1.jpg"
+  },
+  {
+    "revision": "b8bea86eb62f5a5d1a31447ded4b68d9",
+    "url": "/596-project/static/media/truck-1.b8bea86e.jpg"
+  },
+  {
+    "revision": "30dc9e41ad4190ac34b5591d0e7406e6",
+    "url": "/596-project/static/media/truck-3.30dc9e41.jpg"
+  },
+  {
+    "revision": "fc52e7cc6be848a9ef0f05a2e29fd3f0",
+    "url": "/596-project/static/media/daniel-braker.fc52e7cc.jpg"
+  },
+  {
+    "revision": "65c35e6efd1784300878415d5861ec3d",
+    "url": "/596-project/static/media/shodan.65c35e6e.png"
+  },
+  {
+    "revision": "9b731eff9aa69ef90af036f23382ab08",
+    "url": "/596-project/index.html"
+  }
+];
